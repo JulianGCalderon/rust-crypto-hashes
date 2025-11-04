3b17f6e1d54a8a5c65873ad70c25fcce6f863bbc
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-11-04 17:06:06 -0300

    Use inner module in compress

--- blake2/src/macros.rs
+++ blake2/src/macros.rs
@@ -159,15 +159,6 @@ macro_rules! blake2_core_impl {
         }
 
         impl $name {
-            #[inline(always)]
-            fn iv0() -> $vec {
-                $vec::new($IV[0], $IV[1], $IV[2], $IV[3])
-            }
-            #[inline(always)]
-            fn iv1() -> $vec {
-                $vec::new($IV[4], $IV[5], $IV[6], $IV[7])
-            }
-
             /// Creates a new context with the full set of sequential-mode parameters.
             pub fn new_with_params(
                 salt: &[u8],
@@ -250,79 +241,31 @@ macro_rules! blake2_core_impl {
             }
 
             fn compress(&mut self, block: &Block<Self>, f0: $word, f1: $word) {
-                use $crate::consts::SIGMA;
-
-                #[cfg_attr(not(feature = "size_opt"), inline(always))]
-                fn quarter_round(v: &mut [$vec; 4], rd: u32, rb: u32, m: $vec) {
-                    v[0] = v[0].wrapping_add(v[1]).wrapping_add(m.from_le());
-                    v[3] = (v[3] ^ v[0]).rotate_right_const(rd);
-                    v[2] = v[2].wrapping_add(v[3]);
-                    v[1] = (v[1] ^ v[2]).rotate_right_const(rb);
-                }
-
-                #[cfg_attr(not(feature = "size_opt"), inline(always))]
-                fn shuffle(v: &mut [$vec; 4]) {
-                    v[1] = v[1].shuffle_left_1();
-                    v[2] = v[2].shuffle_left_2();
-                    v[3] = v[3].shuffle_left_3();
-                }
-
-                #[cfg_attr(not(feature = "size_opt"), inline(always))]
-                fn unshuffle(v: &mut [$vec; 4]) {
-                    v[1] = v[1].shuffle_right_1();
-                    v[2] = v[2].shuffle_right_2();
-                    v[3] = v[3].shuffle_right_3();
-                }
-
-                #[cfg_attr(not(feature = "size_opt"), inline(always))]
-                fn round(v: &mut [$vec; 4], m: &[$word; 16], s: &[usize; 16]) {
-                    quarter_round(v, $R1, $R2, $vec::gather(m, s[0], s[2], s[4], s[6]));
-                    quarter_round(v, $R3, $R4, $vec::gather(m, s[1], s[3], s[5], s[7]));
-
-                    shuffle(v);
-                    quarter_round(v, $R1, $R2, $vec::gather(m, s[8], s[10], s[12], s[14]));
-                    quarter_round(v, $R3, $R4, $vec::gather(m, s[9], s[11], s[13], s[15]));
-                    unshuffle(v);
-                }
+                // TODO: Can we transmute instead?
+                let h = [
+                    self.h[0].0,
+                    self.h[0].1,
+                    self.h[0].2,
+                    self.h[0].3,
+                    self.h[1].0,
+                    self.h[1].1,
+                    self.h[1].2,
+                    self.h[1].3,
+                ];
 
                 let mut m: [$word; 16] = Default::default();
                 let n = core::mem::size_of::<$word>();
                 for (v, chunk) in m.iter_mut().zip(block.chunks_exact(n)) {
                     *v = $word::from_ne_bytes(chunk.try_into().unwrap());
                 }
-                let h = &mut self.h;
 
-                let t0 = self.t as $word;
-                let t1 = match $bytes::to_u8() {
-                    64 => 0,
-                    32 => (self.t >> 32) as $word,
-                    _ => unreachable!(),
-                };
+                let h = $mod::compress::<{ $mod::ROUNDS }>(h, &m, self.t, f0, f1);
 
-                let mut v = [
-                    h[0],
-                    h[1],
-                    Self::iv0(),
-                    Self::iv1() ^ $vec::new(t0, t1, f0, f1),
+                // TODO: Can we transmute instead?
+                self.h = [
+                    $crate::simd::Simd4::new(h[0], h[1], h[2], h[3]),
+                    $crate::simd::Simd4::new(h[4], h[5], h[6], h[7]),
                 ];
-
-                round(&mut v, &m, &SIGMA[0]);
-                round(&mut v, &m, &SIGMA[1]);
-                round(&mut v, &m, &SIGMA[2]);
-                round(&mut v, &m, &SIGMA[3]);
-                round(&mut v, &m, &SIGMA[4]);
-                round(&mut v, &m, &SIGMA[5]);
-                round(&mut v, &m, &SIGMA[6]);
-                round(&mut v, &m, &SIGMA[7]);
-                round(&mut v, &m, &SIGMA[8]);
-                round(&mut v, &m, &SIGMA[9]);
-                if $bytes::to_u8() == 64 {
-                    round(&mut v, &m, &SIGMA[0]);
-                    round(&mut v, &m, &SIGMA[1]);
-                }
-
-                h[0] = h[0] ^ (v[0] ^ v[2]);
-                h[1] = h[1] ^ (v[1] ^ v[3]);
             }
         }
 
