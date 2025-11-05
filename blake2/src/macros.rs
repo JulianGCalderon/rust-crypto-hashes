eee7bd3a1f912d37ba5e20f673f298096968ed95
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2025-11-05 19:00:47 -0300

    expose block

--- blake2/src/macros.rs
+++ blake2/src/macros.rs
@@ -68,7 +68,7 @@ macro_rules! blake2_impl {
         #[cfg_attr(not(feature = "size_opt"), inline(always))]
         pub fn compress<const ROUNDS: usize>(
             state: &mut [Simd4Word; 2],
-            message: &[Word; 16],
+            block: &[u8; Word::BITS as usize * 2],
             t: u64,
             f0: Word,
             f1: Word,
@@ -108,6 +108,12 @@ macro_rules! blake2_impl {
                 unshuffle(v);
             }
 
+            let mut m: [Word; 16] = Default::default();
+            let n = core::mem::size_of::<Word>();
+            for (v, chunk) in m.iter_mut().zip(block.chunks_exact(n)) {
+                *v = Word::from_ne_bytes(chunk.try_into().unwrap());
+            }
+
             let t0 = t as Word;
             let t1 = match Word::BITS {
                 64 => 0,
@@ -125,23 +131,23 @@ macro_rules! blake2_impl {
             // The compiler doesn't unroll the loop in this case, so we hardcode
             // the most common cases (10 and 12 rounds) to improve performance.
             if ROUNDS == 10 || ROUNDS == 12 {
-                round(&mut v, message, &SIGMA[0]);
-                round(&mut v, message, &SIGMA[1]);
-                round(&mut v, message, &SIGMA[2]);
-                round(&mut v, message, &SIGMA[3]);
-                round(&mut v, message, &SIGMA[4]);
-                round(&mut v, message, &SIGMA[5]);
-                round(&mut v, message, &SIGMA[6]);
-                round(&mut v, message, &SIGMA[7]);
-                round(&mut v, message, &SIGMA[8]);
-                round(&mut v, message, &SIGMA[9]);
+                round(&mut v, &m, &SIGMA[0]);
+                round(&mut v, &m, &SIGMA[1]);
+                round(&mut v, &m, &SIGMA[2]);
+                round(&mut v, &m, &SIGMA[3]);
+                round(&mut v, &m, &SIGMA[4]);
+                round(&mut v, &m, &SIGMA[5]);
+                round(&mut v, &m, &SIGMA[6]);
+                round(&mut v, &m, &SIGMA[7]);
+                round(&mut v, &m, &SIGMA[8]);
+                round(&mut v, &m, &SIGMA[9]);
                 if ROUNDS == 12 {
-                    round(&mut v, message, &SIGMA[0]);
-                    round(&mut v, message, &SIGMA[1]);
+                    round(&mut v, &m, &SIGMA[0]);
+                    round(&mut v, &m, &SIGMA[1]);
                 }
             } else {
                 for i in 0..ROUNDS {
-                    round(&mut v, message, &SIGMA[i % 10]);
+                    round(&mut v, &m, &SIGMA[i % 10]);
                 }
             }
 
@@ -245,13 +251,7 @@ macro_rules! blake2_core_impl {
             }
 
             fn compress(&mut self, block: &Block<Self>, f0: $mod::Word, f1: $mod::Word) {
-                let mut m: [$mod::Word; 16] = Default::default();
-                let n = core::mem::size_of::<$mod::Word>();
-                for (v, chunk) in m.iter_mut().zip(block.chunks_exact(n)) {
-                    *v = $mod::Word::from_ne_bytes(chunk.try_into().unwrap());
-                }
-
-                $mod::compress::<{ $mod::ROUNDS }>(&mut self.h, &m, self.t, f0, f1);
+                $mod::compress::<{ $mod::ROUNDS }>(&mut self.h, block.into(), self.t, f0, f1);
             }
         }
 
